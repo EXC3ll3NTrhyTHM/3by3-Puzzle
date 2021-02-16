5e02b7c900c2c1962115f53f6507094a5cf3ecbb
Blake Simpson <shadowcloud131@gmail.com>
2021-02-16 16:14:48 -0600

    Initial commit

---
+++ src/main/java/Puzzle.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Puzzle {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(String.valueOf(Puzzle.class.getResource("prog1_input.txt")));
+
+    }
+}
