624ecc01d5ba025137b8b66645cc836029c3f912
Blake Simpson <shadowcloud131@gmail.com>
2021-02-16 17:11:48 -0600

    Reading file complete

--- src/main/java/Puzzle.java
+++ src/main/java/Puzzle.java
@@ -1,10 +1,44 @@
+import com.sun.deploy.net.MessageHeader;
+import jdk.nashorn.internal.IntDeque;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Puzzle {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(String.valueOf(Puzzle.class.getResource("prog1_input.txt")));
+    private static State state;
+    public static List<String> currentPuzzle = new ArrayList<String>();
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("src/main/resources/prog1_input.txt"));
+        while (scanner.hasNextLine()) {
+
+            String nextLine = scanner.nextLine();
+            int lineLength = nextLine.length();
+
+            switch (lineLength) {
+                case 2: System.out.println("invalid puzzle");
+                break;
+
+                case 6: currentPuzzle.add(nextLine);
+                break;
+
+                case 0:
+                    for (String line: currentPuzzle) {
+                        System.out.println(line);
+                    }
+                    System.out.println("\n");
+                    currentPuzzle.clear();
+                break;
+
+                default:
+                    System.out.println(lineLength);
+                    break;
+            }
+        }
     }
 }
--- src/main/java/State.java
+++ src/main/java/State.java
@@ -1,2 +1,9 @@
 public class State {
+    public char[][] currentState;
+
+//    public void fillRowOfCurrentState(String input) {
+//        for (int i = 0; i < 3; i++) {
+//            currentState[row][i] = input.charAt(i);
+//        }
+//    }
 }
